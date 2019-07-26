f4c947147cd7cebddf13aa4aa89a4ef4138d0509
Heorhii Siburov <g.siburov@gmail.com>
2019-07-26 10:16:40 -0700

    classes done

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -46,6 +46,7 @@ let cuboid = new CuboidMaker ({
   height: 5
 });
 
+console.log('===Here is test of prototypes===');
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
